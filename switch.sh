03782a0cdd85c2ff291cd8436d5f6c1789b509f6
Abhay Poddar <abhayp@juniper.net>
2022-02-04 11:44:33 +0000

    Add inventory switching

--- switch.sh
+++ switch.sh
@@ -14,3 +14,21 @@ git checkout collections
 
 python --version
 ansible --version
+
+echo "Checking for Hyphen origin inventory backups"
+if test -f "/etc/ansible/hosts.hyphen.bak"; then
+    echo "Underscore origin Inventory backup present"
+else
+    cp /etc/ansible/hosts /etc/ansible/hosts.hyphen.bak
+    echo "Underscore origin Inventory backup created"
+fi
+echo "Attempting switching inventory files"
+if test -f "/etc/ansible/hosts.underscore.bak"; then
+    echo "Underscore origin Inventory backup present, switching"
+    cp /etc/ansible/hosts.underscore.bak /etc/ansible/hosts
+else
+    echo "Underscore Inventory backup not present"
+    cat /etc/ansible/hosts | sed 's/\(\[.*\)-/\1_/g' > /etc/ansible/hosts.underscore.bak
+    cp /etc/ansible/hosts.underscore.bak /etc/ansible/hosts
+    echo "Underscore origin Inventory created and switched"
+fi
