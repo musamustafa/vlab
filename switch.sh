a1ad7f22d4af20c9833e879d374dba0f38c7dcea
Abhay Poddar <abhayp@juniper.net>
2022-02-04 09:54:58 +0000

    add switch.sh to master

---
+++ switch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+_script="$(readlink -f ${BASH_SOURCE[0]})"
+_base="$(dirname $_script)"
+
+echo "Script name : $_script"
+echo "Current working dir : $PWD"
+echo "Script location path (dir) : $_base"
+
+cd $_base
+ln -fs /usr/bin/python3 /usr/bin/python
+source .venv/python3/bin/activate
+git checkout collections
+
+python --version
+ansible --version
