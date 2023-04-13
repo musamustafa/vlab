eb6704682606e5d9d5ae42405f36135fb4022e26
asunny <asunny@juniper.net>
2023-04-13 14:11:07 +0000

    Update switch-to-3.sh

--- python-switch/switch-to-3.sh
+++ python-switch/switch-to-3.sh
@@ -6,12 +6,13 @@ _base="$(dirname $_script)"
 echo "Script name : $_script"
 echo "Current working dir : $PWD"
 echo "Script location path (dir) : $_base"
+_cwd="$PWD"
 
 cd $_base
 ln -fs /usr/bin/python3 /usr/bin/python
 source ~/.venv/python3/bin/activate
 git checkout collections
 
-cd $PWD
+cd $_cwd
 python --version
 ansible --version
