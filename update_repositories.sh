1000a764b2908c31709187fd9bf01fc7e7ed97cc
asunny <asunny@juniper.net>
2023-05-02 16:36:56 +0000

    Update update_repositories.sh

--- update_repositories.sh
+++ update_repositories.sh
@@ -19,7 +19,7 @@ git_path_jcl="--git-dir=$ansible_playbook_path/.git --work-tree=$ansible_playboo
 ##################################### user_input for repository list
 repo_array=()
 echo -e "Enter the repo clone URLs (SSH or HTTPS) for the repos that you want to update:
-(Press Enter + ctrl+D at the end of the list)\n"
+(Press Enter, then, Ctrl+D at the end of the list)\n"
 
 while read repo
 do
@@ -55,8 +55,9 @@ for repo_path in  "$default_path"/*; do
 
    cp -rf $ansible_playbook_path/{activate.sh,ensure_kvm_running.yml,install_vxlan_linux_host.yml,README.md,switch.sh,ansible.cfg,get_config_from_device.yml,push_directory_to_git.yml,requirements.txt,upgrade_junos.yml,group_vars,python_scripts,roles,docs,install_config_to_device.yml,python_version_check.yml,shell_scripts} $repo_path
 
-   echo -e "\nCompleted updating local repo *for $repo_path."
-   echo -e "Enter "yes" if you want to push updates to Git *for $repo_path: "
+   echo -e "\n***For $repo_path:"
+   echo -e "\nCompleted updating local repo."
+   echo -e "Enter "yes" if you want to push updates to Git for $repo_path: "
 
 ######################################### git push 
    read user_input
@@ -66,7 +67,7 @@ for repo_path in  "$default_path"/*; do
       git $git_path add --all
       git $git_path commit -m "Updated Ansible playbooks from latest Python3 version of JCL_Ansible_Playbooks using update_repositories.sh"
       git $git_path push
-      echo -e "\nGit commit and push SUCCESSFUL *for $repo_path."
+      echo -e "\nGit commit and push SUCCESSFUL for $repo_path."
       rm -rf $repo_path
    else
       echo -e "\nGit push ABORTED *for $repo_path !"  
